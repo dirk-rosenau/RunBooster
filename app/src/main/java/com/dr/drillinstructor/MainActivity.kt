265169e102b714b2b11d61733360cbb8f34828da
Dirk Rosenau <dirk@skillhero.de>
2020-06-24 21:34:56 +0200

    Play with sounds

--- app/src/main/java/com/dr/drillinstructor/MainActivity.kt
+++ app/src/main/java/com/dr/drillinstructor/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.dr.drillinstructor
 
 import android.content.Context
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.VibrationEffect
 import android.os.Vibrator
 import android.support.wearable.activity.WearableActivity
-import com.dr.drillinstructor.R
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : WearableActivity() {
+
 
     private var playState = false;
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,12 +27,33 @@ class MainActivity : WearableActivity() {
 
     private fun togglePlay() {
         playState = !playState
-        if (playState) {
-            play_button.setImageResource(R.drawable.ic_stop)
-        } else {
+        if (!playState) {
             play_button.setImageResource(R.drawable.ic_play_arrow)
+            playSound("outstanding.mp3")
+        } else {
+            play_button.setImageResource(R.drawable.ic_stop)
+            playSound("gogogo.mp3")
         }
         vibrate()
+    }
+
+    private fun playSound(filename: String) {
+        try {
+            val m = MediaPlayer()
+            val descriptor = assets.openFd(filename)
+            m.setDataSource(
+                descriptor.fileDescriptor,
+                descriptor.startOffset,
+                descriptor.length
+            )
+            descriptor.close()
+            m.prepare()
+            m.setVolume(1f, 1f)
+            m.isLooping = false
+            m.start()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     private fun vibrate() {
